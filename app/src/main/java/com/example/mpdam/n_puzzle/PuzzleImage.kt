c2a1a74d0a5684c7673f96493700ff7902072c6a
WajdiGridha <wajdigridha744@gmail.com>
2023-01-04 17:47:33 +0100

    first commit

---
+++ app/src/main/java/com/example/mpdam/n_puzzle/PuzzleImage.kt
@@ -0,0 +1,17 @@
+package com.example.mpdam.n_puzzle
+
+enum class PuzzleImage(val drawableId: Int) {
+
+    DJERBA(R.drawable.djerba),
+
+    CENTRE_VILLE_TUNISIE(R.drawable.centre_ville),
+
+    SIDI_BOU(R.drawable.sidi_bou),
+
+    TBARKA(R.drawable.tbarka),
+
+    NABEUL(R.drawable.nabeul),
+
+
+    CUSTOM(-1)
+}
